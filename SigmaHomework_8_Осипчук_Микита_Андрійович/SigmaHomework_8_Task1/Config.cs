20396edfd21fde2a6c2d31d5c1b373e179bac377
modeod <modeod.mdd@gmail.com>
2022-12-06 17:07:56 +0300

    added Config file

---
+++ SigmaHomework_8_Осипчук_Микита_Андрійович/SigmaHomework_8_Task1/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigmaHomework_8_Task1
+{
+    internal static class Config
+    {
+        public static string OrderFileName { get; set; } = "order.txt";
+
+        public static string SubstitutionsFileName { get; set; } = "substitutions.txt";
+
+        public static string ResultFileName { get; set; } = "result.txt";
+    }
+}
